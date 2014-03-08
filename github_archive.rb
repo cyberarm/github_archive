18a8073e3b4bdbad65654ef45657dfe7fa98c7e1
Cyberarm <matthewlikesrobots@gmail.com>
2014-03-08 16:05:35 -0600

    Downloader now only downloads last 4o days of data.

--- github_archive.rb
+++ github_archive.rb
@@ -4,10 +4,14 @@ require "open-uri"
 require "zlib"
 require "time"
 require "gruff"
+
+require_relative "lib/download" if ARGV[0] == "download"
+
 require_relative "lib/parser"
-#require_relative "lib/download"
 require_relative "lib/graph"
 
-@graph = GithubArchive::Graph.new
-@graph.process_data
-@graph.render
+if Integer(ARGV[0])
+  @graph = GithubArchive::Graph.new
+  @graph.process_data
+  @graph.render
+end
--- lib/download.rb
+++ lib/download.rb
@@ -1,29 +1,32 @@
-# All most all of this file was originally created by:
+# Inspired by:
 # http://datasyndrome.com/post/51657080886/downloading-and-processing-the-github-data
 
-def prepend(number)
-  return number <= 9 ? ("0" + number.to_s) : number.to_s
-end
+require "date"
+@time_now = Date.today
+
 @start_time = Time.now
-for year in ['14'] do
-  for month in (1..12) do
-    month = prepend(month)
-    for day in (1..31) do
-      day = prepend(day)
-      unless File.exist?("data/temp/20#{year}-#{month}-#{day}-23.json.gz")
-        begin
-          @raw_data = open("http://data.githubarchive.org/20#{year}-#{month}-#{day}-23.json.gz").read
-          File.open("data/temp/20#{year}-#{month}-#{day}-23.json.gz", "wb") do |file|
-            file.write @raw_data
-          end
-         rescue OpenURI::HTTPError =>e
-          puts e
-         end
-      else
-        puts "Skipped file...\n\n"
+40.times do |i|
+  @time = (@time_now-i).to_time
+  year = @time.year
+  month = @time.strftime('%m')
+  day = @time.strftime('%d')
+
+  for hour in (0..23) do
+    unless File.exist?("data/temp/#{year}-#{month}-#{day}-#{hour}.json.gz")
+      begin
+        @raw_data = open("http://data.githubarchive.org/#{year}-#{month}-#{day}-#{hour}.json.gz").read
+        File.open("data/temp/#{year}-#{month}-#{day}-#{hour}.json.gz", "wb") do |file|
+          file.write @raw_data
+        end
+        puts "Downloaded: #{year}-#{month}-#{day}-#{hour}.json.gz"
+      rescue OpenURI::HTTPError =>e
+        p e
       end
+    else
+      puts "Skipped file...\n\n"
     end
   end
 end
+
 @end_time = Time.now
 puts "Time to complete: #{Time.now-@start_time}"
